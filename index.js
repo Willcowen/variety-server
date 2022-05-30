2fa326440f9167332a1fad8852c7f25f74a9f68b
willyc <willcowendrums@gmail.com>
2022-05-30 12:30:35 +0100

    Added a Procfile.

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use('/task', taskRouter)
 app.use('/tasks', tasksRouter)
 
 //Start up our server
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
  console.log(`Server is running on http://localhost:${port}/`)
 })
