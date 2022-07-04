8f4d0986f631f6eeb40ecae7b1fadd7cc8d8ac1a
willyc <willcowendrums@gmail.com>
2022-07-04 16:25:26 +0100

    removed dummy endpoint

--- src/controllers/shoes.js
+++ src/controllers/shoes.js
@@ -2,7 +2,6 @@ const shoes = require('../../shoes.json')
 const prisma = require('../utils/prisma')
 
 const getShoes = async (req, res) => {
-// res.status(200).json(shoes)
 
 const shoesList = await prisma.shoe.findMany({})
 console.log('shoesList:', shoesList)
